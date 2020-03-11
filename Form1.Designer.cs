eea9f2825ba7c79be5141517bbc45fddf7990c6f
Nikita <nik.kulik2001@mail.ru>
2020-03-11 16:55:11 +0300

    123

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -103,9 +103,9 @@ namespace Курсач
             this.labelTimer.Show();
 
 
-            // 
+            
             // Form1
-            // 
+            
             this.BackColor = Color.FromArgb(226, 218, 241);
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
